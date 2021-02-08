6036d226abfd0e3fe8e2fe8bea08685be65f447b
Faisal Ahmed <fftfaisal@gmail.com>
2021-02-08 18:03:05 +0600

    create plugin index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Plugin Name: Authoes Video Embed
+ * Plugin URI: https://github.com/fftfaisal/authoes-video-embed/
+ * Description: Embed authoes videos into wordpress,woocommerce site.Also Generate  thumbnail from autheos from Video,EAN ID then save into wordpress and use as post thumbnail automatically.
+ * Author: Faisal Ahmed
+ * Author URI: https://faisal.com.bd
+ * Version: 1.0.0
+ * Text Domain: authoes-embed
+ * WC requires at least: 3.6.1
+ * WC tested up to: 4.9.2
+ */
